805562c9f414adf536f9b1226108172475bba099
Alex David Garcia Chala <garciaalexdavidchala009@gmail.com>
2026-05-26 12:47:55 -0500

    Backend conectado a Supabase

--- db.js
+++ db.js
@@ -1,19 +1,18 @@
-// backend/db.js
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production'
-    ? { rejectUnauthorized: false }
-    : false,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.connect((err, client, release) => {
   if (err) {
     console.error('❌ Error conectando a PostgreSQL:', err.stack);
   } else {
-    console.log('✅ Conectado a PostgreSQL online');
+    console.log('✅ Conectado a Supabase PostgreSQL');
     release();
   }
 });
