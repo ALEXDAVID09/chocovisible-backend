7134b9d5a73c03dab373132701000c3a2c677fac
Alex David Garcia Chala <garciaalexdavidchala009@gmail.com>
2026-05-26 07:49:01 -0500

    Usar DATABASE_URL en db

--- db.js
+++ db.js
@@ -3,24 +3,23 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    max: 20, // máximo de conexiones
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
 
 // Probar conexión
 pool.connect((err, client, release) => {
-    if (err) {
-        console.error('❌ Error conectando a PostgreSQL:', err.stack);
-    } else {
-        console.log('✅ Conectado a PostgreSQL - Base de datos:', process.env.DB_NAME);
-        release();
-    }
+  if (err) {
+    console.error('❌ Error conectando a PostgreSQL:', err.stack);
+  } else {
+    console.log('✅ Conectado a PostgreSQL online');
+    release();
+  }
 });
 
 module.exports = pool;
