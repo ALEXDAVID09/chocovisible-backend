3e17cc76f70e49994ee79db71a091574edff69be
Alex David Garcia Chala <garciaalexdavidchala009@gmail.com>
2026-05-26 08:12:11 -0500

    Corregir conexión Supabase

--- db.js
+++ db.js
@@ -1,25 +1,20 @@
-// backend/db.js
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-// Probar conexión
 pool.connect((err, client, release) => {
-  if (err) {
-    console.error('❌ Error conectando a PostgreSQL:', err.stack);
-  } else {
-    console.log('✅ Conectado a PostgreSQL online');
-    release();
-  }
+    if (err) {
+        console.error('❌ Error conectando a PostgreSQL:', err.stack);
+    } else {
+        console.log('✅ Conectado a PostgreSQL');
+        release();
+    }
 });
 
 module.exports = pool;
